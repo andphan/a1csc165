8db8d1bba61ba497932af81e6981d59aa7bb41ba
andphan <amannamedphan@gmail.com>
2015-02-03 00:31:50 -0800

    more githubbing

---
+++ src/a1/Treasurehuntmain.java
@@ -0,0 +1,12 @@
+package a1;
+
+
+public class Treasurehuntmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+}
---
+++ src/a1/treasurehunt2014.java
@@ -0,0 +1,28 @@
+package a1;
+import sage.app.BaseGame;
+import sage.display.*;
+import sage.display.DisplaySystem;
+import sage.scene.SceneNode;
+import sage.scene.shape.Rectangle;
+
+import graphicslib3D.Point3D;
+
+import java.awt.event.*;
+import java.util.Random;
+import java.awt.Color;
+import java.text.DecimalFormat;
+
+
+public class treasurehunt2014 {
+
+
+		public void initGame()
+		{
+			// overwritten
+		}
+		public void update(float ElapsedTimeMS)
+		{
+			// overwritten
+		}
+		
+	}
