1d43cae347e3795b2a5f0686fdc2db88976302ea
andphan <amannamedphan@gmail.com>
2015-02-07 17:04:07 -0800

    update. need to add input classes

--- src/a1/treasurehunt2015.java
+++ src/a1/treasurehunt2015.java
@@ -41,76 +41,33 @@ public class treasurehunt2015 extends BaseGame {
 			IInputManager im = getInputManager();
 			// initialize Managers
 			System.out.println("initGame call");
-				
-			//keyboard and gamepad inputs
-			String kbName = im.getKeyboardName();
-			// String gpName = im.getFirstGamepadName();
+			initGameObjects();	
 			
-			// actions
-			IAction quitGame = new QuitGameAction(this);
-			
-			// action associations
-			im.associateAction(kbName, net.java.games.input.Component.Identifier.Key.ESCAPE,
-					quitGame, IInputManager.INPUT_ACTION_TYPE.ON_PRESS_ONLY);
-			// end
+			//
 		}
 			
 		public void initGameObjects()
 		{
 			display = getDisplaySystem();
 			display.setTitle("hey");
-			camera = display.getRenderer().getCamera();
-			camera.setPerspectiveFrustum(45, 1, 0.01, 1000);
-			camera.setLocation(new Point3D(1,1, 20));
 			
-			/*
-			rect = new Rectangle();
-			Matrix3D rectM = rect.getLocalTranslation();
-			rectM.translate(-1, 1, -5);
-			rect.setLocalTranslation(rectM);
-			addGameWorldObject(rect);
+			// score
+			timeD = new HUDString("Time: " + time);
+			addGameWorldObject(timeD);
+			scoreD = new HUDString("Score: " + time);
+			addGameWorldObject(scoreD);
 			
-			aPyr = new myNewTriMesh();
-			Matrix3D pyrM = aPyr.getLocalTranslation();
-			pyrM.translate(2, 2, -8);
-			aPyr.setLocalTranslation(pyrM);
-			addGameWorldObject(aPyr);
-			
-			*/
-			Point3D origin = new Point3D(0, 0, 0);
-			Point3D xEnd = new Point3D(100, 0, 0);
-			Point3D yEnd = new Point3D(0, 100, 0);
-			Point3D zEnd = new Point3D(0, 0, 100);
-			Line xAxis = new Line (origin, xEnd, Color.red, 2);
-			Line yAxis = new Line (origin, yEnd, Color.green, 2);
-			Line zAxis = new Line (origin, zEnd, Color.blue, 2);
-			addGameWorldObject(xAxis);
-			addGameWorldObject(yAxis);
-			addGameWorldObject(zAxis);
-			
+			// 	
+			Rectangle rect1 = new Rectangle();
+			addGameWorldObject(rect1);
 		}
 		public void update(float ElapsedTimeMS)
 		{
 			// overwritten
-			
+			super.update(ElapsedTimeMS);
 			// move camera
 			
-		
 		}
 		
-			// must handle input actions sage.input.action.IAction, InputManager.associateAction()
-		// A/D/W/S
-		// left arrow / right - rotate camera around it's yaw
-		// up / down arrow - rotate camera around its pitch
-		// escape - quit game s
-		
-		// controller input
-		
-		public class leftTurn extends AbstractInputAction
-		{
-			public void performAction(float time, Event e)
-			{
-				
-			}
-		}
+		// add controller get
 	}
