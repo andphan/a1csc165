4ac7ac2e5c2bc726de3c96a4897dfb53093bbc8f
andphan <amannamedphan@gmail.com>
2015-02-20 01:09:10 -0800

    push!

---
+++ src/a1/MovementToggle.java
@@ -0,0 +1,18 @@
+package a1;
+
+import sage.input.action.AbstractInputAction;
+import net.java.games.input.Event;
+
+public class MovementToggle extends AbstractInputAction {
+	private boolean moving = false;
+	
+	public boolean isMoving()
+	{
+		return moving;
+	}
+	public void performAction(float t, Event e)
+	{
+		moving = !moving;
+	}
+
+}
